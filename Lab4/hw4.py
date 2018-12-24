898e7bfcd200169fa2f91daed80d4efe49879c52
leeang6969 <leeang6969.cs05@g2.nctu.edu.tw>
2018-12-24 16:22:57 +0800

    Finish Forward Propagation

--- Lab4/hw4.py
+++ Lab4/hw4.py
@@ -10,22 +10,34 @@ data = loadmat('data/hw4.mat') # type(data) = dict
 from sklearn.preprocessing import OneHotEncoder
 encoder = OneHotEncoder(sparse=False)
 y_onehot = encoder.fit_transform(data['y'])
-'''
+
 def sigmoid(z):
     return 1 / (1 + np.exp(-z))
-    
+
 def forward_propagate(X, theta1, theta2):
     m = X.shape[0]
 
     #Write codes here
-    a1 = 
-    z2 =
-    a2 = 
-    z3 = 
-    h = 
+    a1 = X
+    theta1_tranpose = theta1.transpose()
+    theta1_bias = theta1_tranpose[theta1_tranpose.shape[0] - 1]
+    theta1_tranpose = np.delete(theta1_tranpose, theta1_tranpose.shape[0] - 1, axis=0)
+    z2 = np.dot(X, theta1_tranpose)
+    for i in range(0, X.shape[0]):
+        z2[i] = z2[i] + theta1_bias
+    activate = lambda i : sigmoid(i)
+    vectorized_activate = np.vectorize(activate)
+    a2 = vectorized_activate(z2)
+    theta2_tranpose = theta2.transpose()
+    theta2_bias = theta2_tranpose[theta2_tranpose.shape[0] - 1]
+    theta2_tranpose = np.delete(theta2_tranpose, theta2_tranpose.shape[0] - 1, axis=0)
+    z3 = np.dot(a2, theta2_tranpose)
+    for i in range(0, a2.shape[0]):
+        z3[i] = z3[i] + theta2_bias
+    h = vectorized_activate(z3)
     
     return a1, z2, a2, z3, h
-    
+
 def cost(params, input_size, hidden_size, num_labels, X, y, learning_rate):
     m = X.shape[0] # 5000
     X = np.matrix(X)
@@ -58,14 +70,14 @@ m = data['X'].shape[0]
 X = np.matrix(data['X'])
 y = np.matrix(data['y'])
 # unravel the parameter array into parameter matrices for each layer
-theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1))))
-theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1))))
+theta1 = np.matrix(np.reshape(params[:hidden_size * (input_size + 1)], (hidden_size, (input_size + 1)))) #10*401
+theta2 = np.matrix(np.reshape(params[hidden_size * (input_size + 1):], (num_labels, (hidden_size + 1)))) #10*11
 
 a1, z2, a2, z3, h = forward_propagate(X, theta1, theta2)
 
 def sigmoid_gradient(z):
     return np.multiply(sigmoid(z), (1 - sigmoid(z)))    
-
+'''
 def backprop(params, input_size, hidden_size, num_labels, X, y, learning_rate):
     m = X.shape[0]
    
